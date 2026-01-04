6f1f4724513589d4d7c4e36b4e199518f5e9c96e
Amresh Kumar <kamresh485@gmail.com>
2026-01-04 12:36:42 +0530

    expression Input deletion

--- frontend/src/components/expressionInput/expressionInput.js
+++ frontend/src/components/expressionInput/expressionInput.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Select } from "../Select/select";
 
 const EditorMode = {
@@ -19,6 +19,8 @@ export const ExpressionInput = ({
   const [selecteExpressionValue, setSelecteExpressionValue] = useState("");
   const [expressionSearchValue, setExpressionSearchValue] = useState("");
 
+  const inputRef = useRef(null);
+
   const showExpressionSelect = editorMode === EditorMode.EXPRESSION;
   const hasSelectedExpression = selectedExpressionIndex > -1;
 
@@ -26,12 +28,142 @@ export const ExpressionInput = ({
     ? options.filter((option) => option.includes(expressionSearchValue))
     : options;
 
+  function deleteExpression(deleteIndex) {
+    const selectedExpressionsCopy = [...selectedExpressions];
+    const deletedExpression = selectedExpressionsCopy.splice(deleteIndex, 1)[0];
+    const deletedValuelength =
+      deletedExpression.endIndex - deletedExpression.startIndex + 4; /*{{}} */
+    const lengthChange = deletedValuelength * -1;
+    updateExpressionIndexes(selectedExpressionsCopy, lengthChange, deleteIndex);
+    selectedExpressionsChange(selectedExpressionsCopy);
+    const newText =
+      value.slice(0, deletedExpression.startIndex - 2) +
+      value.slice(deletedExpression.endIndex + 2);
+    onChange(newText);
+  }
+
   const handlekeyPress = (e) => {
     const key = e.key;
+    const isExpressionMode = editorMode === EditorMode.EXPRESSION;
+    const caretPosition = e.target.selectionStart;
 
-    if (key === "{" && editorMode === EditorMode.EXPRESSION) {
-      // don't let user write another opening bracket
+    /**
+     * when user has typed {{ don't let him write another opening bracket
+     * we identify this by checking last char and if expression mode is active
+     */
+    if (key === "{" && isExpressionMode) {
       e.preventDefault();
+    }
+
+    /*
+      deleting an expression
+      when an expression is seleced and user presses backspoace or delete
+    */
+    if (key === "Backspace" || (key === "Delete" && isExpressionMode)) {
+      if (selectedExpressionIndex > -1) {
+        e.preventDefault();
+        const selectedExpression = selectedExpressions[selectedExpressionIndex];
+        deleteExpression(selectedExpressionIndex);
+        e.target.setSelectionRange(
+          selectedExpression.startIndex,
+          selectedExpression.startIndex
+        );
+        setEditorMode(EditorMode.STRING);
+        return;
+      }
+    }
+
+    /**
+     * user trying to delete whole value by pressing backspace,
+     * if user starts to delete a espression node something like {{espression}}|
+     * select whole expression first, then user can pres backspace or delete to delete it (code is above)
+     * or if user presses left right, let user edit the expression
+     */
+    if (key === "Backspace" && value.endsWith("}")) {
+      const closestExpressionIndex = selectedExpressions.findIndex(
+        (expression) => caretPosition === expression.endIndex + 2
+      );
+      if (closestExpressionIndex === -1) {
+        // in case input starts with }
+        return;
+      }
+      // don't let user delete the closing bracket
+      e.preventDefault();
+      const selectedExpression = selectedExpressions[closestExpressionIndex];
+      e.target.setSelectionRange(
+        selectedExpression.startIndex,
+        selectedExpression.endIndex
+      );
+      setEditorMode(EditorMode.EXPRESSION);
+      setSelectedExpressionIndex(closestExpressionIndex);
+      setSelecteExpressionValue(selectedExpression.value);
+    }
+
+    const leftKey = "ArrowLeft";
+    const rightKey = "ArrowRight";
+    const leftKeyPressed = key === leftKey;
+    const rightKeyPressed = key === rightKey;
+
+    const leftOrRightPressed = leftKeyPressed || rightKeyPressed;
+    const aboutToEnterExpression = leftKeyPressed
+      ? value[caretPosition - 1] === "}"
+      : rightKeyPressed
+      ? value[caretPosition + 1] === "}"
+      : false;
+
+    /**
+     * when user navigates using left or right key and expression mode is not active,
+     * when user finds an expression either start or end eg |{{expression}} or {{expression}}|
+     * then select whole expression
+     * then user can press backspace or delete to delete it (code is above)
+     * or he can selct a new expression
+     */
+    if (leftOrRightPressed && !isExpressionMode && aboutToEnterExpression) {
+      e.preventDefault();
+      let closestExpressionIndex = -1;
+      if (key === "ArrowLeft") {
+        closestExpressionIndex = selectedExpressions.findIndex(
+          (expression) => caretPosition === expression.endIndex + 2
+        );
+      } else if (key === "ArrowRight") {
+        closestExpressionIndex = selectedExpressions.findIndex(
+          (expression) => caretPosition === expression.startIndex - 2
+        );
+      }
+      if (closestExpressionIndex > -1) {
+        const selectedExpression = selectedExpressions[closestExpressionIndex];
+        e.target.setSelectionRange(
+          selectedExpression.startIndex,
+          selectedExpression.endIndex
+        );
+        setEditorMode(EditorMode.EXPRESSION);
+        setSelectedExpressionIndex(closestExpressionIndex);
+        setSelecteExpressionValue(selectedExpression.value);
+      }
+    }
+
+    const caretIsInsideExpression =
+      isExpressionMode && selectedExpressionIndex > -1;
+    /**
+     * if an expression is already selected and user presses left or right
+     * then move the caret inside the expression
+     * if right key is pressed then move the caret to the end of the expression
+     * if left key is pressed then move the caret to the start of the expression
+     */
+    if (leftOrRightPressed && caretIsInsideExpression) {
+      e.preventDefault();
+      const selectedExpression = selectedExpressions[selectedExpressionIndex];
+      if (leftKeyPressed) {
+        e.target.setSelectionRange(
+          selectedExpression.startIndex,
+          selectedExpression.startIndex
+        );
+      } else if (rightKeyPressed) {
+        e.target.setSelectionRange(
+          selectedExpression.endIndex,
+          selectedExpression.endIndex
+        );
+      }
     }
   };
 
@@ -41,7 +173,7 @@ export const ExpressionInput = ({
     if (editorMode === EditorMode.EXPRESSION) {
       if (val.length < value.length) {
         // ie user has pressed backspace
-        // we no longer want to be in expression mode, cuz valid us like abc {
+        // we no longer want to be in expression mode, cuz val us like abc {
         setEditorMode(EditorMode.STRING);
         setExpressionSearchValue("");
         onChange(val);
@@ -71,14 +203,14 @@ export const ExpressionInput = ({
     onChange(val);
   };
 
-  const updateExpressionIndexes = (lengthChange) => {
-    for (
-      let i = selectedExpressionIndex + 1;
-      i < selectedExpressions.length;
-      i++
-    ) {
-      selectedExpressions[i].startIndex += lengthChange;
-      selectedExpressions[i].endIndex += lengthChange;
+  const updateExpressionIndexes = (
+    expressions,
+    lengthChange,
+    index = selectedExpressionIndex + 1
+  ) => {
+    for (let i = index; i < expressions.length; i++) {
+      expressions[i].startIndex += lengthChange;
+      expressions[i].endIndex += lengthChange;
     }
   };
 
@@ -98,7 +230,7 @@ export const ExpressionInput = ({
         startIndex: oldExpression.startIndex,
         endIndex: oldExpression.endIndex + lengthChange,
       };
-      updateExpressionIndexes(lengthChange);
+      updateExpressionIndexes(selectedExpressionsCopy, lengthChange);
       selectedExpressionsChange(selectedExpressionsCopy);
       onChange(newText);
     } else {
@@ -148,6 +280,7 @@ export const ExpressionInput = ({
   return (
     <>
       <input
+        ref={inputRef}
         type="text"
         value={value + expressionSearchValue}
         onChange={handleChange}
